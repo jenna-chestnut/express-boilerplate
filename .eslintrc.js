206ee0f33bb4c2f739564756b630593e0a2994d1
Jenna Chestnut <jennabot5000@gmail.com>
2020-11-30 11:07:18 -0600

    modified eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,24 @@
 module.exports = {
+  /** 
+    * ESLint: http://eslint.org/docs/user-guide/configuring
+    */
+
+  // "env:" supplies predefined global variables
   env: {
     node: true,
+    mocha: true,
+    es6: true,
+    mongo: true,
     commonjs: true,
     es2021: true,
   },
+
+  globals: {
+    supertest: true,
+    expect: true
+  },
+
+  // our configuration extends the recommended base configuration, & prettier
   extends: [
     "eslint:recommended",
     "prettier",
@@ -15,8 +30,19 @@ module.exports = {
     "prettier/unicorn",
     "prettier/vue",
   ],
+
   parserOptions: {
     ecmaVersion: 12,
   },
-  rules: {},
+
+  //ESLint rules: Severity Levels: off = 0 | warn = 1 | error = 2
+  rules: {
+    "eqeqeq": "warn",              //prefer strict equality `===`
+    "no-console": "warn",          //allows but warn about console like `console.log()`
+    "no-unused-vars": ["warn", { "varsIgnorePattern": "^_" }],
+    "no-eval": "error",            //disallows `eval()` usage
+    "indent": ["error", 2],        //enforce 2 space indents (not tabs)        
+    "semi": ["error", "always"],   //enforce semi-colon usage
+    "quotes": [0]
+  },
 };
